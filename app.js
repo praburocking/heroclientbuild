82cf488dff9b9502592e57607e8a991da6aa5006
Prabu <prabumohan96@gmail.com>
2020-01-16 10:10:57 +0530

    google verification

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(express.static('build'))
 app.use(cors())
 
+// app.get('googleb73f2055947ef46b.html',(req,res)=>{
+//   console.log(__dirname+'/build/index.html');
+//   res.sendFile(path.join(__dirname+'/build/index.html'));
+// })
 
 app.get('*', (req,res) =>{
   console.log(__dirname+'/build/index.html');
